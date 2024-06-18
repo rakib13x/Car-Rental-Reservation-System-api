8ee79b89a84f65e7e214940b517c537bead80c5c
rakib13x <rakib13x@gmail.com>
2024-06-18 21:26:36 +0600

    Car Model interface added

--- src/app/interface/car.interface.ts
+++ src/app/interface/car.interface.ts
@@ -1,1 +1,10 @@
-
+export interface TCar {
+  name: string;
+  description: string;
+  color: string;
+  isElectric: boolean;
+  status: 'available' | 'not available';
+  features: string[];
+  pricePerHour: number;
+  isDeleted: boolean;
+}
