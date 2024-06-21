aad3aa1b3f0212401c9fbb1817dc4a7096ca244b
rakib13x <rakib13x@gmail.com>
2024-06-21 12:32:34 +0600

    authorization token with bearer functionality added

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,10 +1,39 @@
 import { NextFunction, Request, Response } from 'express';
-
+import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+import AppError from '../errors/AppError';
 import { TUserRole } from '../interface/user.interface';
+import { User } from '../model/user.model';
 import catchAsync from '../utils/catchAsync';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    const { role, userEmail } = decoded;
+
+    const user = await User.isUserExistsByEmail(userEmail);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+    req.user = decoded as JwtPayload;
     next();
   });
 };
